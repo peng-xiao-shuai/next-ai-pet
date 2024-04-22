571820988e5c81b692b81745533593f814748710
peng-xiao-shuai <1612565136@qq.com>
2024-04-22 17:32:46 +0800

    pref: 判断是否需要创建宠物

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,9 +1,37 @@
-import { ClientCreatePet } from './_components/ClientCreatePet';
+'use client';
+import { useEffect, useState } from 'react';
+import { Client } from './_components/Client';
+import { ClientCreatePet } from './_components/CreatePet';
+import { Navbar } from './_components/Navbar';
+import Cookies from 'js-cookie';
+import { Loading } from '@/components/Loading';
+import { useUserStore } from '@/hooks/use-user';
 
 export default function Home() {
+  const { userState } = useUserStore();
+  const [loading, setLoading] = useState(true);
+  const [isPet, setIsPet] = useState(false);
+  useEffect(() => {
+    const isPet = Number(Cookies.get('isPet'));
+    if (!Number.isNaN(isPet)) {
+      setIsPet(isPet > 0);
+      setLoading(false);
+    }
+  }, [userState]);
+
   return (
     <>
-      <ClientCreatePet></ClientCreatePet>
+      {loading ? (
+        <Loading></Loading>
+      ) : !isPet ? (
+        <ClientCreatePet></ClientCreatePet>
+      ) : (
+        <div className="bg-black">
+          <Navbar></Navbar>
+
+          <Client></Client>
+        </div>
+      )}
     </>
   );
 }
