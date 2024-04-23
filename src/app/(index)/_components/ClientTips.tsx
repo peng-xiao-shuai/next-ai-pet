126c625c35c22304bc2b3a0dad06346c1247deba
peng-xiao-shuai <1612565136@qq.com>
2024-04-23 16:36:25 +0800

    style

--- src/app/(index)/_components/ClientTips.tsx
+++ src/app/(index)/_components/ClientTips.tsx
@@ -7,7 +7,7 @@ export const ClientTips: FC<{
 }> = ({ text, className, cornerClassName }) => {
   return (
     <div
-      className={`absolute px-5 py-4 bg-gradient-to-t to-[#F4B4CF] from-[#BE89F5] rounded-xl text-[#1D1C21] text-lg leading-5 font-bold ${className}`}
+      className={`absolute z-50 px-5 py-4 bg-gradient-to-t to-[#F4B4CF] from-[#BE89F5] rounded-xl text-[#1D1C21] text-lg leading-5 font-bold ${className}`}
     >
       {text}
       {/* 三角形 */}
