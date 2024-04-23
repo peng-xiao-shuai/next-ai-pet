a3bc12c350d18d65a9e3c8ebbdc3a8681f118f04
peng-xiao-shuai <1612565136@qq.com>
2024-04-23 15:19:32 +0800

    pref: 新增聊天文件，判断是否存在宠物

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -6,9 +6,11 @@ import { Navbar } from './_components/Navbar';
 import Cookies from 'js-cookie';
 import { Loading } from '@/components/Loading';
 import { useUserStore } from '@/hooks/use-user';
+import { fetchRequest } from '@/utils/request';
 
 export default function Home() {
   const { userState } = useUserStore();
+  const [friendId, setFriendId] = useState<string | undefined>(undefined);
   const [loading, setLoading] = useState(true);
   const [isPet, setIsPet] = useState(false);
   useEffect(() => {
@@ -16,20 +18,25 @@ export default function Home() {
     if (!Number.isNaN(isPet)) {
       setIsPet(isPet > 0);
       setLoading(false);
+
+      fetchRequest('/restApi/friend/list/v2').then(({ result }) => {
+        if (result.conversations.rows.length) {
+          setFriendId(result.conversations.rows[0].id);
+        }
+      });
     }
   }, [userState]);
-
   return (
     <>
       {loading ? (
         <Loading></Loading>
       ) : !isPet ? (
-        <ClientCreatePet></ClientCreatePet>
+        <ClientCreatePet setFriendId={setFriendId}></ClientCreatePet>
       ) : (
-        <div className="bg-black">
+        <div className="bg-black h-full flex flex-col">
           <Navbar></Navbar>
 
-          <Client></Client>
+          <Client friendId={friendId}></Client>
         </div>
       )}
     </>
