c8fed2f9897be224bd74ab6215dd7b57e75a10b0
peng-xiao-shuai <1612565136@qq.com>
2024-04-24 16:06:42 +0800

    pref: 添加动画

--- src/app/(index)/_components/ClientTips.tsx
+++ src/app/(index)/_components/ClientTips.tsx
@@ -1,19 +1,35 @@
 import { FC } from 'react';
+import { m } from 'framer-motion';
 
 export const ClientTips: FC<{
   text: string;
   className?: string;
   cornerClassName?: string;
-}> = ({ text, className, cornerClassName }) => {
+  visible: boolean;
+}> = ({ text, visible, className, cornerClassName }) => {
   return (
-    <div
-      className={`absolute z-50 px-5 py-4 bg-gradient-to-t to-[#F4B4CF] from-[#BE89F5] rounded-xl text-[#1D1C21] text-lg leading-5 font-bold ${className}`}
+    <m.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={
+        visible
+          ? { opacity: 1, scale: 1 }
+          : {
+              opacity: 0,
+              transitionEnd: {
+                display: 'none',
+              },
+            }
+      }
     >
-      {text}
-      {/* 三角形 */}
       <div
-        className={`absolute bg-[#BE89F5] size-3 rotate-45 ${cornerClassName}`}
-      ></div>
-    </div>
+        className={`absolute z-50 px-5 py-4 bg-gradient-to-t to-[#F4B4CF] from-[#BE89F5] rounded-xl text-[#1D1C21] text-lg leading-5 font-bold ${className}`}
+      >
+        {text}
+        {/* 三角形 */}
+        <div
+          className={`absolute bg-[#BE89F5] size-3 rotate-45 ${cornerClassName}`}
+        ></div>
+      </div>
+    </m.div>
   );
 };
