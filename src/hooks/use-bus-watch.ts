44be98e70159e23397618e45f8dfdbf68e13002a
peng-xiao-shuai <1612565136@qq.com>
2024-04-25 19:44:22 +0800

    pref: 添加泛型

--- src/hooks/use-bus-watch.ts
+++ src/hooks/use-bus-watch.ts
@@ -1,19 +1,21 @@
 import { useEffect } from 'react';
 import bus from '@/utils/bus';
 import { Handler } from 'mitt';
-export const useBusWatch = (
+export const useBusWatch = <T = any>(
   _type: string,
-  _handler: Handler,
+  _handler: Handler<T>,
   beforeUnload: () => void = () => {}
 ) => {
   useEffect(() => {
     // 启用监听、
+    // @ts-ignore
     bus.on(_type, _handler);
 
     // 在组件卸载之前移除监听
     return () => {
       beforeUnload();
 
+      // @ts-ignore
       bus.off(_type, _handler);
     };
   }, [_type, _handler, beforeUnload]);
