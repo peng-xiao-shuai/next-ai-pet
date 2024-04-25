3816666a023dc11b530d76c96958208650110bd5
peng-xiao-shuai <1612565136@qq.com>
2024-04-25 15:56:00 +0800

    pref: 对接喂食接口

--- src/app/(index)/_components/ClientFeedDrawer.tsx
+++ src/app/(index)/_components/ClientFeedDrawer.tsx
@@ -3,23 +3,38 @@ import { Button } from '@/components/Button';
 import Image from 'next/image';
 import { AiFillQuestionCircle } from 'react-icons/ai';
 import {
-  Drawer,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-} from '@/components/ui/drawer';
-import { Dispatch, FC, SetStateAction, useState } from 'react';
-import { IoCloseCircle } from 'react-icons/io5';
+  Dispatch,
+  FC,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { cn } from '@/lib/utils';
 import { ClientChatDrawer } from './ClientChatDrawer';
+import { useUserStore } from '@/hooks/use-user';
+import { fetchRequest } from '@/utils/request';
+import { ChatContext } from './Client';
+import { toast } from 'sonner';
 
 export const ClientFeedDrawer: FC<{
   drawerVisible: boolean;
   setDrawerVisible: Dispatch<SetStateAction<boolean>>;
 }> = ({ drawerVisible, setDrawerVisible }) => {
+  const { state } = useContext(ChatContext);
   const [feedValue, setFeedValue] = useState(20);
+  const { userState, setDataLocal } = useUserStore();
   const [sendLoading, setSendLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState('You don’t have enough food');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    if (Number(feedValue) > userState.walletAble) {
+      setErrorMsg('You don’t have enough food');
+    } else {
+      setErrorMsg('');
+    }
+  }, [feedValue, userState.walletAble]);
+
   return (
     <ClientChatDrawer
       drawerVisible={drawerVisible}
@@ -58,17 +73,12 @@ export const ClientFeedDrawer: FC<{
         ))}
       </div>
 
-      <div
-        className={cn(
-          'flex justify-between items-center gap-4 text-white',
-          errorMsg ? 'mb-4' : 'mb-10'
-        )}
-      >
-        <div>
+      <div className={cn('flex justify-between gap-4 text-white', 'mb-10')}>
+        <div className="relative flex-1">
           <div
             className={cn(
-              'bg-[#1D1C21] rounded-2xl h-12 flex items-center px-2 flex-1 border-[#FF2F53]',
-              errorMsg ? 'border-2 mb-[6px]' : ''
+              'bg-[#1D1C21] rounded-2xl h-12 flex items-center px-2 w-full border-2 border-transparent duration-300 transition-all',
+              errorMsg ? 'border-[#FF2F53] mb-[6px]' : ''
             )}
           >
             <input
@@ -82,11 +92,18 @@ export const ClientFeedDrawer: FC<{
           </div>
 
           {Boolean(errorMsg) && (
-            <div className="text-[#FF2F53] text-xs">{errorMsg}</div>
+            <div className="text-[#FF2F53] text-xs absolute">{errorMsg}</div>
           )}
         </div>
 
-        <span className="text-lg">Feed all</span>
+        <div
+          className="text-lg leading-[3]"
+          onClick={() => {
+            setFeedValue(userState.walletAble);
+          }}
+        >
+          Feed all
+        </div>
       </div>
 
       <div className="flex items-center justify-between text-white">
@@ -99,7 +116,7 @@ export const ClientFeedDrawer: FC<{
                 height={16}
                 alt="feed"
               ></Image>
-              <span className="text-xs">234</span>
+              <span className="text-xs">{userState.walletAble}</span>
             </div>
             <AiFillQuestionCircle className="size-5 text-[#737373]"></AiFillQuestionCircle>
           </div>
@@ -108,9 +125,32 @@ export const ClientFeedDrawer: FC<{
 
         <Button
           title="Send"
-          click={() => {}}
+          click={async () => {
+            if (errorMsg) {
+              return;
+            }
+
+            setSendLoading(true);
+            try {
+              await fetchRequest(
+                `/restApi/friend/feedFood/${state!
+                  .friendId!}?quantity=${feedValue}`
+              );
+
+              toast('投喂成功！');
+            } catch (err: any) {
+              console.log(err);
+            }
+            setSendLoading(false);
+            setDataLocal({
+              walletAble: userState.walletAble - feedValue,
+            });
+          }}
           disabled={sendLoading}
-          className="!w-28 h-11 bg-gradient-to-r to-[#D18EF7] from-[#FA3B67] mb-0"
+          className={cn(
+            'duration-300 transition-all !w-28 h-11 bg-gradient-to-r to-[#D18EF7] from-[#FA3B67] mb-0',
+            errorMsg ? 'grayscale' : ''
+          )}
         ></Button>
       </div>
     </ClientChatDrawer>
