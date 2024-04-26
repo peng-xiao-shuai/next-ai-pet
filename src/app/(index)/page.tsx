3ce6f7320e412cb56949c6a35e352534873118ef
peng-xiao-shuai <1612565136@qq.com>
2024-04-26 15:55:10 +0800

    pref: 优化首屏判断

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -13,12 +13,18 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const [isPet, setIsPet] = useState(false);
   useEffect(() => {
-    const isPet = Number(Cookies.get('isPet'));
-    if (!Number.isNaN(isPet)) {
-      setIsPet(isPet > 0);
+    if (friendId) {
+      setLoading(false);
+      setIsPet(true);
+      return;
+    }
+
+    const petCount = Number(Cookies.get('isPet'));
+    if (!Number.isNaN(petCount)) {
+      setIsPet(petCount > 0);
       setLoading(false);
 
-      if (isPet > 0 && !friendId) {
+      if (petCount > 0) {
         fetchRequest('/restApi/friend/list/v2').then(({ result }) => {
           if (result.conversations.rows.length) {
             setFriendId(result.conversations.rows[0].id);
