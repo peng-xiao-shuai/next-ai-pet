28c18d1fafc444650bdf29bc292aa391dba476a4
彭小黑 <1612565136@qq.com>
2024-05-07 03:01:03 +0000

    fix: 修复滚动问题

--- src/components/TGInitScript.tsx
+++ src/components/TGInitScript.tsx
@@ -73,27 +73,13 @@ export const TGInitScript = () => {
   };
 
   useEffect(() => {
-    let ts: number | undefined;
-    const onTouchStart = (e: TouchEvent) => {
-      ts = e.touches[0].clientY;
-    };
-    const onTouchMove = (e: TouchEvent) => {
-      // if (scrollableEl) {
-      //   const scroll = scrollableEl.scrollTop;
-      //   const te = e.changedTouches[0].clientY;
-      //   if (scroll <= 0 && ts! < te) {
-      //     e.preventDefault();
-      //   }
-      // } else {
-      e.preventDefault();
-      // }
-    };
-    document.documentElement.addEventListener('touchstart', onTouchStart, {
-      passive: false,
-    });
-    document.documentElement.addEventListener('touchmove', onTouchMove, {
-      passive: false,
-    });
+    // https://github.com/deptyped/vue-telegram/issues/11#issuecomment-1999265843
+    const overflow = 100
+    document.body.style.overflowY = 'hidden'
+    document.body.style.marginTop = `${overflow}px`
+    document.body.style.height = window.innerHeight + overflow + "px"
+    document.body.style.paddingBottom = `${overflow}px`
+    window.scrollTo(0, overflow)
   }, []);
   return (
     <>
