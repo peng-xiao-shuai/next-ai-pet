3ae6d09efdd7d2d2b6ed181f7a5890604ad37bbc
peng-xiao-shuai <1612565136@qq.com>
2024-05-07 18:53:30 +0800

    pref: 更改分享逻辑

--- src/hooks/use-share.ts
+++ src/hooks/use-share.ts
@@ -9,22 +9,27 @@ export const useShare = () => {
     const str = JSON.stringify({
       id: userState.googleOpenid,
     });
-    fetchRequest(
-      '/telegram/sendMessage/' +
-        window.Telegram?.WebApp?.initDataUnsafe?.user?.id,
-      {
-        message: `点击当前连接转发 tg://msg_url?url=t.me/pxs_test_bot/test?startapp=${base64UrlEncode(
-          str
-        )}&text=快来和我一起领养AI宠物吧！`,
-      }
-    );
+    // fetchRequest(
+    //   '/telegram/sendMessage/' +
+    //     window.Telegram?.WebApp?.initDataUnsafe?.user?.id,
+    //   {
+    //     message: `点击当前连接转发 tg://msg_url?url=t.me/pxs_test_bot/test?startapp=${base64UrlEncode(
+    //       str
+    //     )}&text=快来和我一起领养AI宠物吧！`,
+    //   }
+    // );
 
-    copyText(
-      `t.me/pxs_test_bot/test?startapp=${base64UrlEncode(str)}`,
-      () => {
-        toast('复制链接成功，也可以在机器人聊天中点击链接分享');
-      },
-      false
+    // copyText(
+    //   `t.me/pxs_test_bot/test?startapp=${base64UrlEncode(str)}`,
+    //   () => {
+    //     toast('复制链接成功，也可以在机器人聊天中点击链接分享');
+    //   },
+    //   false
+    // );
+    window.Telegram.WebApp.openTelegramLink(
+      `https://t.me/share/url?url=t.me/pxs_test_bot/test?startapp=${base64UrlEncode(
+        str
+      )}&text=🎁Let's play-to-earn aridrop right now!`
     );
   };
 
