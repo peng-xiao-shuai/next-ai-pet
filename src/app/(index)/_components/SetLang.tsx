3c2477d5b980c1f267e396484a438c8578641764
peng-xiao-shuai <1612565136@qq.com>
2024-05-14 00:24:13 +0800

    fix: 尝试修复

--- src/app/(index)/_components/SetLang.tsx
+++ src/app/(index)/_components/SetLang.tsx
@@ -1,20 +1,15 @@
 'use client';
 
 import * as React from 'react';
-import { FaLanguage } from 'react-icons/fa6';
-import { DropdownMenuCheckboxItemProps } from '@radix-ui/react-dropdown-menu';
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { MdOutlineLanguage } from 'react-icons/md';
 import { useTranslation } from '@/hooks/useTranslation';
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
 
 export function SetLang() {
   const { lang, setLang } = useTranslation();
@@ -22,40 +17,22 @@ export function SetLang() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline">
-          <Image
-            src="/icons/lang.png"
-            alt="lang"
-            width={24}
-            height={24}
-          ></Image>
-        </Button>
+        <Image src="/icons/lang.png" alt="lang" width={24} height={24}></Image>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-white !min-w-20">
-        <DropdownMenuItem
-          onClick={() => {
-            setLang('zh');
-            location.reload();
+        <DropdownMenuRadioGroup
+          value={lang}
+          onValueChange={(v: string) => {
+            if (lang !== v) {
+              setLang(v as typeof lang);
+              location.reload();
+            }
           }}
         >
-          中文
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            setLang('en');
-            location.reload();
-          }}
-        >
-          English
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            setLang('ru');
-            location.reload();
-          }}
-        >
-          Pyccknn
-        </DropdownMenuItem>
+          <DropdownMenuRadioItem value="zh">中文</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="en">English</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="ru">Pyccknn</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
