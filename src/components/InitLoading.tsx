c2cd27f822bba64de960b8266e7a755b5c89bb78
peng-xiao-shuai <1612565136@qq.com>
2024-05-21 22:53:34 +0800

    pref: 改为背景图

--- src/components/InitLoading.tsx
+++ src/components/InitLoading.tsx
@@ -4,22 +4,10 @@ import { useState, useEffect, ReactNode, FC } from 'react';
 
 const BgLoading = (children?: ReactNode) => {
   return (
-    <div className="fixed z-50 top-0 w-full h-[100vh]">
+    <div className="fixed z-50 top-0 w-full h-[100vh] bg-[url(/images/start-bg-image.png)] bg-center bg-[size:100vw_100vh]">
       <div className="absolute z-[51] text-4xl font-bold top-8 text-white text-center w-full font-sans">
         Chat to Earn!
       </div>
-      <Image
-        width={750}
-        height={1624}
-        sizes="100vw"
-        style={{
-          width: '100%',
-          height: 'auto',
-        }}
-        src="/images/start-bg-image.png"
-        alt="start"
-        priority
-      />
 
       {children}
     </div>
