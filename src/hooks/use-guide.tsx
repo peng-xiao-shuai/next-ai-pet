a5894598de8da01373d587f946201d118004dd5f
peng-xiao-shuai <1612565136@qq.com>
2024-05-21 16:46:47 +0800

    pref: 引导补充

--- src/hooks/use-guide.tsx
+++ src/hooks/use-guide.tsx
@@ -2,20 +2,27 @@ import { useTour } from '@reactour/tour';
 import { useEffect } from 'react';
 
 export const useGuide = ({ list }: { list: any[] }) => {
-  const { setIsOpen, setCurrentStep } = useTour();
+  const { setIsOpen, setCurrentStep, currentStep } = useTour();
 
   useEffect(() => {
     /**
      * 这里要配置好顺序， 第一个是“操作按钮”，第二个是“喂狗粮”
      * 那么在 page.tsx 中 step 数组中的 selector 对应的值也应该是 “操作按钮”上的 类名或者id名称
+     *
+     * 第一个空对应 page step 中的第一个
      */
-    const index = ['HIGHLIGHT_ACTION_BTN', 'HIGHLIGHT_FEED_FOOD'].indexOf(
+    const index = ['', 'HIGHLIGHT_ACTION_BTN', 'HIGHLIGHT_FEED_FOOD'].indexOf(
       list![list!.length - 1]?.specialEventTrigger
     );
-    if (index != -1) {
-      // document.body.setAttribute('data-guide', 'HIGHLIGHT_ACTION_BTN');
-      setCurrentStep(1);
+
+    if (index > -1) {
+      document.body.setAttribute(
+        'data-guide',
+        list![list!.length - 1]?.specialEventTrigger
+      );
+      setCurrentStep(index);
       setIsOpen(true);
     }
-  }, [list, setCurrentStep, setIsOpen]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [list]);
 };
