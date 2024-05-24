0ffeb360c72c214c237a2f5d1db635214f4c0c3f
peng-xiao-shuai <1612565136@qq.com>
2024-05-24 12:00:09 +0800

    pref: dev 不自动播放

--- src/app/(index)/_components/ControlSound.tsx
+++ src/app/(index)/_components/ControlSound.tsx
@@ -35,9 +35,8 @@ export const ControlSound = () => {
   };
 
   useEffect(() => {
-    bodyClick();
-
     if (process.env.NODE_ENV != 'development') {
+      bodyClick();
       document.body.addEventListener('click', bodyClick);
     }
 
